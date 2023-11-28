42e4a8a5eb16b1731e9d3db9b8a395ffe632def8
jongho Kim <31331701+eodl123@users.noreply.github.com>
2023-11-29 08:18:40 +0900

    로그인 초반부 수정기입

--- main.py
+++ main.py
@@ -60,6 +60,11 @@ async def get_image(item_id):
                               """).fetchone()[0]
     return Response(content=bytes.fromhex(image_bytes))
 
+@app.post('/signup')
+def signup(id:Annotated[str,Form()], password:Annotated[str,Form()]):
+     print(id,password)
+     return '200'
+
 app.mount("/", StaticFiles(directory="frontend", html=True), name="frontend")
 
 
