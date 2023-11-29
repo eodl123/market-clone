5978f7f50c43acf630bec0edea9cd1dc0c21d8e7
jongho Kim <31331701+eodl123@users.noreply.github.com>
2023-11-30 02:37:07 +0900

    로그인 인증 기능 추가

---
+++ frontend/login.js
@@ -0,0 +1,21 @@
+const form = document.querySelector("#login-form");
+
+let accessToken = null;
+
+const handleSubmit = async (event) => {
+  event.preventDefault();
+  const formData = new FormData(form);
+  const sha256Password = sha256(formData.get("password"));
+  formData.set("password", sha256Password);
+
+  const res = await fetch("/login", {
+    method: "post",
+    body: formData,
+  });
+  const data = await res.json();
+  const accessToken = data.access_token;
+  window.localStorage.setItem("token", accessToken); //로컬 스토리지만들어놓은거
+  alert("로그인 되었습니다!");
+  window.location.pathname = "/";
+};
+form.addEventListener("submit", handleSubmit);
